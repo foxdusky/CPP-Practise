cd52ae83b0f2c3011e0391a7f68d6c616796d5ee
fox <mr.dusky.fox@gmail.com>
2022-02-15 00:48:41 +0300

    feat: Practise 12

---
+++ Practise 12/Fibonachi/Fibonachi/Fibonachi.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+long int f(int n)
+{
+	if (n == 0) return 0;
+	if (n == 1 || n == 2 || n == -1 || n == -2) return 1;
+	if (n > 0) {
+		return f(n - 1) + f(n - 2);
+	}
+	else {
+		return f(n + 1) + f(n + 2);
+	}
+}
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int x = 0;
+	cout << "Введите желаемое порядковое число Фибоначчи: ";
+	cin >> x;
+	int fib, mn = 1;
+	fib = f(x);
+	if (x < 0 && x % 2 == 0) {
+		mn = -1;
+	}
+	cout << x << "-е Число Фибоначчи: " << mn*fib << "\n";
+}	
